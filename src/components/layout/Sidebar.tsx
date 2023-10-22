bded09ad0b76b82d41cf3f27737eeb96d6b54f46
Freddy L <neo-ciber_94@hotmail.com>
2023-10-22 16:56:49 -0400

    Updated sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,6 +3,23 @@
 import { isomorphicClient } from "@/lib/utils/isomorphic.client";
 import { logOut, useSession } from "../providers/SessionProvider";
 import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/solid";
+import ChatBubbleOvalLeftEllipsisIcon from "@heroicons/react/24/outline/esm/ChatBubbleOvalLeftEllipsisIcon";
+
+const conversations: string[] = [
+  "Meeting with the Team",
+  "Planning the Project",
+  "Lunch with Friends",
+  "Job Interview",
+  "Family Reunion",
+  "Movie Night",
+  "Travel Plans",
+  "Planning an Around-the-World Adventure Travel Itinerary",
+  "Monthly Book Club Discussion: 'The Catcher in the Rye' by J.D. Salinger",
+  "Attending International Tech Conference: TechCon 2023",
+  "Book Club Discussion",
+  "Tech Conference",
+  "Coffee with Colleagues",
+];
 
 export default function Sidebar() {
   const { session } = useSession();
@@ -16,8 +33,32 @@ export default function Sidebar() {
           isOpen ? "border-rainbow-bottom w-10/12 border-r sm:w-[300px]" : "w-0"
         }`}
       >
-        <div className="flex h-full flex-col p-4">
-          <span>Sidebar</span>
+        <div className="flex h-full flex-col p-4 group">
+          <div className="flex flex-row items-center gap-3 border-b border-b-red-500 pb-4 text-base">
+            <ChatBubbleOvalLeftEllipsisIcon
+              className="h-8 w-8"
+              style={{
+                transform: `rotateY(180deg)`,
+              }}
+            />
+            <span>Conversations</span>
+          </div>
+
+          <div className="flex h-full flex-col gap-2 overflow-y-scroll hover:overflow-y-auto pr-2 py-2 conversations-scrollbar">
+            {conversations.map((conversation, idx) => {
+              return (
+                <button
+                  key={idx}
+                  className="shadow-inset flex flex-row items-center rounded-md p-4
+                   text-left text-sm shadow-white/40 hover:bg-neutral-900"
+                >
+                  <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+                    {conversation}
+                  </span>
+                </button>
+              );
+            })}
+          </div>
 
           {session && (
             <div className="mt-auto border-t border-t-violet-600 pt-4">
