c6e1ecb12bcc1deb45baf08c7b031abaa8825ace
Freddy L <neo-ciber_94@hotmail.com>
2023-10-22 16:33:58 -0400

    Updated chat input

--- src/app/chat/ChatInput.tsx
+++ src/app/chat/ChatInput.tsx
@@ -35,7 +35,7 @@ export default function ChatInput({ onSend }: ChatInputProps) {
         placeholder="Send Message"
         rows={1}
         value={text}
-        className="w-full resize-none overflow-hidden rounded-lg border border-neutral-400 bg-black py-4 pl-4 pr-10 text-white
+        className="w-full resize-none overflow-hidden rounded-lg border border-gray-400/30 bg-black py-4 pl-4 pr-10 text-white
          shadow-lg outline-none placeholder:text-white/50 focus:border-neutral-400/60 dark:border-neutral-400/30 lg:w-[700px]"
         onKeyDown={(e) => {
           if (e.key === "Enter" && !e.shiftKey) {
