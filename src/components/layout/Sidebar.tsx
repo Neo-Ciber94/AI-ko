f15c3817ace19f398182f29db9227d8a12079a04
Freddy L <neo-ciber_94@hotmail.com>
2023-10-22 18:23:21 -0400

    minor changes

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -33,24 +33,26 @@ export default function Sidebar() {
           isOpen ? "border-rainbow-bottom w-10/12 border-r sm:w-[300px]" : "w-0"
         }`}
       >
-        <div className="flex h-full flex-col p-4 group">
-          <div className="flex flex-row items-center gap-3 border-b border-b-red-500 pb-4 text-base">
-            <ChatBubbleOvalLeftEllipsisIcon
-              className="h-8 w-8"
-              style={{
-                transform: `rotateY(180deg)`,
-              }}
-            />
-            <span>Conversations</span>
+        <div className="group flex h-full flex-col p-4">
+          <div className="border-b border-b-red-500 flex flex-row">
+            <button className="flex w-full flex-row items-center gap-3 rounded-lg mb-2 px-4 py-2 text-base hover:bg-neutral-900">
+              <ChatBubbleOvalLeftEllipsisIcon
+                className="h-8 w-8"
+                style={{
+                  transform: `rotateY(180deg)`,
+                }}
+              />
+              <span>New Conversation</span>
+            </button>
           </div>
 
-          <div className="flex h-full flex-col gap-2 overflow-y-scroll hover:overflow-y-auto pr-2 py-2 conversations-scrollbar">
+          <div className="conversations-scrollbar flex h-full flex-col gap-2 overflow-y-scroll py-2 pr-2 hover:overflow-y-auto">
             {conversations.map((conversation, idx) => {
               return (
                 <button
                   key={idx}
                   className="shadow-inset flex flex-row items-center rounded-md p-4
-                   text-left text-sm shadow-white/40 hover:bg-neutral-900"
+                   text-left text-sm shadow-white/20 hover:bg-neutral-900"
                 >
                   <span className="overflow-hidden text-ellipsis whitespace-nowrap">
                     {conversation}
