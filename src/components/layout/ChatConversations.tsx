62584225387c906b310aca2042c53adf9211f636
Freddy L <neo-ciber_94@hotmail.com>
2023-10-23 00:04:57 -0400

    removed spinner from chat conversations

--- src/components/layout/ChatConversations.tsx
+++ src/components/layout/ChatConversations.tsx
@@ -3,7 +3,6 @@
 import { deleteConversation } from "./actions.server";
 import Link from "next/link";
 import { useAction } from "next-safe-action/hook";
-import Spinner from "../Spinner";
 import { useParams } from "next/navigation";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { useConversations } from "../providers/ConversationsProvider";
@@ -14,48 +13,40 @@ export default function ChatConversations() {
   const conversations = useConversations();
 
   return (
-    <>
-      {false ? (
-        <div className="flex h-full w-full flex-row items-center justify-center p-4">
-          <Spinner />
-        </div>
-      ) : (
-        <div className="conversations-scrollbar flex h-full flex-col gap-2 overflow-y-auto py-2 pr-1">
-          {conversations.map((conversation, idx) => {
-            return (
-              <Link
-                key={idx}
-                href={`/chat/${conversation.id}`}
-                className={`shadow-inset flex flex-row items-center justify-between rounded-md p-4
+    <div className="conversations-scrollbar flex h-full flex-col gap-2 overflow-y-auto py-2 pr-1">
+      {conversations.map((conversation, idx) => {
+        return (
+          <Link
+            key={idx}
+            href={`/chat/${conversation.id}`}
+            className={`shadow-inset flex flex-row items-center justify-between rounded-md p-4
                   text-left text-sm shadow-white/20 hover:bg-neutral-900
                   ${
                     conversationId === conversation.id
                       ? "bg-neutral-900"
                       : "hover:bg-neutral-900"
                   }`}
-              >
-                <span className="overflow-hidden text-ellipsis whitespace-nowrap">
-                  {conversation.title}
-                </span>
+          >
+            <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+              {conversation.title}
+            </span>
 
-                <button
-                  title="Delete Conversation"
-                  className="hover:text-red-500"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    deleteConversationAction.execute({
-                      conversationId: conversation.id,
-                    });
-                  }}
-                >
-                  <TrashIcon className="h-5 w-5 opacity-80" />
-                </button>
-              </Link>
-            );
-          })}
-        </div>
-      )}
-    </>
+            <button
+              title="Delete Conversation"
+              className="hover:text-red-500"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                deleteConversationAction.execute({
+                  conversationId: conversation.id,
+                });
+              }}
+            >
+              <TrashIcon className="h-5 w-5 opacity-80" />
+            </button>
+          </Link>
+        );
+      })}
+    </div>
   );
 }
