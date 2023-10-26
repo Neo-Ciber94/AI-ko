e684e7d238468576693bba0d5f4280b53986fe8d
Freddy L <neo-ciber_94@hotmail.com>
2023-10-26 00:21:38 -0400

    format messages with markdown

--- apps/ai-chat-bot/src/app/chat/ChatMessages.tsx
+++ apps/ai-chat-bot/src/app/chat/ChatMessages.tsx
@@ -1,49 +1,123 @@
-import { type ConversationMessage } from "./actions.server";
+import "highlight.js/styles/github-dark.min.css";
+import { type ConversationMessage } from "@/lib/actions/conversation-messages";
+import markdownIt from "markdown-it";
+import hljs from "highlight.js";
+
+type MarkdownIt = ReturnType<typeof markdownIt>;
+type Message = Pick<ConversationMessage, "id" | "content" | "role">;
+type Role = Message["role"];
 
 type ChatMessagesProps = {
-  messages: Omit<ConversationMessage, "conversationId" | "createdAt">[];
+  messages: Message[];
 };
 
-export default function ChatMessages({ messages }: ChatMessagesProps) {
+export default function ChatMessages(props: ChatMessagesProps) {
+  const messages = formatMessages(props.messages);
+
   return (
     <div className="flex flex-col gap-4 pt-4">
       {messages.map((message) => {
+        const role = message.role;
+
         return (
           <div
             key={message.id}
             className="flex flex-row items-center gap-4 px-8"
           >
-            {message.role === "system" && (
-              <div
-                className="flex h-10 w-10 flex-shrink-0 flex-row items-center justify-center rounded-lg border-2 border-red-500
-              bg-black text-white"
-              >
-                AI
-              </div>
-            )}
-            <div
-              className={`w-full px-4 py-8 ${
-                message.role === "user"
-                  ? "chat-bubble-user"
-                  : "chat-bubble-system"
-              }`}
-            >
-              {message.content}
-            </div>
-
-            {message.role === "user" && (
-              <div
-                className="flex h-10 w-10 flex-shrink-0 flex-row items-center justify-center rounded-lg 
-                border-2 border-blue-500 bg-black text-white"
-              >
-                Me
-              </div>
-            )}
+            {role === "system" && <Avatar role={role}>AI</Avatar>}
+            <MessageContent message={message} />
+            {role === "user" && <Avatar role={role}>Me</Avatar>}
           </div>
         );
       })}
 
-      <div className="h-40 px-4"></div>
+      <div className="h-40 px-4">{/* Some extra padding */}</div>
+    </div>
+  );
+}
+
+function formatMessages(messages: Message[]) {
+  const md: MarkdownIt = markdownIt({
+    langPrefix: "```",
+    highlight: (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          const highlighted = hljs.highlight(str, {
+            language: lang,
+            ignoreIllegals: true,
+          }).value;
+
+          return `<div class="my-4 flex flex-col">
+          <div class="flex flex-row">
+          <span class="my-2 text-white bg-gray-800 px-2 py-2 rounded-lg text-xs w-fit">
+            ${lang}
+          </span>
+          </div>
+            <pre class="hljs p-4 rounded-lg"><code>${highlighted}</code></pre>
+          </div>`;
+        } catch {
+          //
+        }
+      }
+
+      const html = md.utils.escapeHtml(str);
+      return `<pre class="hljs p-4 rounded-lg my-4"><code>${html}</code></pre>`;
+    },
+  });
+
+  // https://github.com/markdown-it/markdown-it/issues/269#issuecomment-506199293
+  md.renderer.rules.fence = function (tokens, idx, options) {
+    const token = tokens[idx];
+    const info = token.info ? md.utils.unescapeAll(token.info).trim() : "";
+    let langName = "";
+    let highlighted;
+
+    if (info) {
+      langName = info.split(/\s+/g)[0];
+    }
+
+    if (options.highlight) {
+      highlighted =
+        options.highlight(token.content, langName, "") ||
+        md.utils.escapeHtml(token.content);
+    } else {
+      highlighted = md.utils.escapeHtml(token.content);
+    }
+
+    return highlighted + "\n";
+  };
+
+  return messages.map((msg) => {
+    const formattedContent = md.render(msg.content);
+    return {
+      ...msg,
+      content: formattedContent,
+    };
+  });
+}
+
+function MessageContent({ message }: { message: Message }) {
+  return (
+    <div
+      className={`w-full px-4 py-8 ${
+        message.role === "user" ? "chat-bubble-user" : "chat-bubble-system"
+      }`}
+      dangerouslySetInnerHTML={{
+        __html: message.content,
+      }}
+    ></div>
+  );
+}
+
+function Avatar({ role, children }: { role: Role; children: React.ReactNode }) {
+  return (
+    <div
+      className={`flex h-10 w-10 flex-shrink-0 flex-row items-center justify-center rounded-lg 
+      border-2 bg-black text-white ${
+        role === "user" ? "border-blue-500" : "border-red-500"
+      }`}
+    >
+      {children}
     </div>
   );
 }
