ca8eb85b8b15e716658b1450d4d0ccb36d5e0c8d
Freddy L <neo-ciber_94@hotmail.com>
2023-10-26 19:58:50 -0400

    removed next-safe-action

--- apps/ai-chat-bot/src/components/ChatConversations.tsx
+++ apps/ai-chat-bot/src/components/ChatConversations.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useAction } from "next-safe-action/hook";
 import { useParams } from "next/navigation";
 import { TrashIcon } from "@heroicons/react/24/outline";
 
