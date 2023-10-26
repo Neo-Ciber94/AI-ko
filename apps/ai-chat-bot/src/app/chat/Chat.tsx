fbc0522490e45549cc8d01aa34521e99e77db96b
Freddy L <neo-ciber_94@hotmail.com>
2023-10-26 00:39:18 -0400

    fix scroll

--- apps/ai-chat-bot/src/app/chat/Chat.tsx
+++ apps/ai-chat-bot/src/app/chat/Chat.tsx
@@ -4,12 +4,13 @@ import ChatInput from "./ChatInput";
 import ChatMessages from "./ChatMessages";
 
 import { useParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useChat } from "@/client/hooks/use-chat";
 import { type ConversationMessage } from "@/lib/actions/conversation-messages";
 
 export default function Chat(props: { messages: ConversationMessage[] }) {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [loaded, setLoaded] = useState(false);
   const { conversationId } = useParams<{ conversationId: string }>();
   const { chat, isLoading, messages } = useChat({
     conversationId,
@@ -27,7 +28,10 @@ export default function Chat(props: { messages: ConversationMessage[] }) {
     }
   };
 
-  useEffect(() => scrollToBottom(), []);
+  useEffect(() => {
+    scrollToBottom();
+    setLoaded(true);
+  }, []);
 
   const handleChat = async (message: string) => {
     await chat(message);
@@ -37,7 +41,9 @@ export default function Chat(props: { messages: ConversationMessage[] }) {
     <>
       <div
         ref={containerRef}
-        className="relative h-full w-full overflow-y-auto"
+        className={`relative h-full w-full ${
+          loaded ? "visible overflow-y-auto" : "invisible overflow-y-scroll"
+        }`}
       >
         {messages.length === 0 ? (
           <div
