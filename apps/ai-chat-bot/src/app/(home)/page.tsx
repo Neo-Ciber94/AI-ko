fff95491f09e4410ab9faa349612954972369c66
Freddy L <neo-ciber_94@hotmail.com>
2023-11-05 22:02:26 -0400

    Updated home page

--- apps/ai-chat-bot/src/app/(home)/page.tsx
+++ apps/ai-chat-bot/src/app/(home)/page.tsx
@@ -16,15 +16,20 @@ export default function HomePage() {
         ability to generate stunning images at your command.
       </p>
 
-      <hr className="my-4 h-1 w-full border-black/20 dark:border-white" />
+      <hr className="my-4 h-1 w-full border-black/20 dark:border-white/20" />
 
-      <Link
-        href="/chat"
-        className="flex flex-row items-center gap-2 rounded-xl bg-black px-10 py-4 text-base text-white shadow-md sm:text-lg"
-      >
-        <span>Chat</span>
-        <ChatBubbleLeftRightIcon className="h-6 w-6" />
-      </Link>
+      <div>
+        <Link
+          href="/chat"
+          className="before:bg-rainbow-right relative flex flex-row items-center gap-2 rounded-xl bg-black
+            px-16 py-3 text-base text-white shadow-md before:absolute before:left-1/2 before:top-1/2 
+            before:-z-10 before:h-[calc(100%+2.5px)] before:w-[calc(100%+2.5px)] 
+            before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-xl sm:text-lg"
+        >
+          <span>Chat</span>
+          <ChatBubbleLeftRightIcon className="h-6 w-6" />
+        </Link>
+      </div>
     </div>
   );
 }
--- apps/ai-chat-bot/tailwind.config.ts
+++ apps/ai-chat-bot/tailwind.config.ts
@@ -12,8 +12,10 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        rainbow:
-          "linear-gradient( to bottom, #ff0000, #ff7f00, #ffff00, #00ff00, #0000ff, #4b0082, #8b00ff)",
+        "rainbow-bottom":
+          "linear-gradient(to bottom, #ff0000, #ff7f00, #ffff00, #00ff00, #0000ff, #4b0082, #8b00ff)",
+        "rainbow-right":
+          "linear-gradient(to right, #ff0000, #ff7f00, #ffff00, #00ff00, #0000ff, #4b0082, #8b00ff)",
       },
       screens: {
         xs: "475px",
