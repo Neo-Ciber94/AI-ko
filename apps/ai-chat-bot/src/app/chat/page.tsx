e1c10ba2a3a48fd3f9f78e4b3d1e79ecc3f0f712
Freddy L <neo-ciber_94@hotmail.com>
2023-11-21 11:06:29 -0400

    minor changes

--- apps/ai-chat-bot/src/app/chat/page.tsx
+++ apps/ai-chat-bot/src/app/chat/page.tsx
@@ -7,7 +7,7 @@ export default function ChatPage() {
         className="mt-5 flex flex-row items-center gap-2 text-center
           font-mono text-2xl font-bold text-gray-500 opacity-60 dark:opacity-30"
       >
-        <ExclamationTriangleIcon className="w-1 h-12 h-12" />
+        <ExclamationTriangleIcon className="w-12 h-12" />
         <span>AIko may provide incorrect information</span>
       </p>
     </div>
